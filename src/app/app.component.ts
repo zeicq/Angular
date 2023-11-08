d421c91228a792f6edad16850e43e4da78fe2160
zeicq <patryk-zielinski@outlook.com>
2023-11-08 20:41:29 +0100

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { TabService } from './services/tab.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  isResetRoute: boolean = false;
+
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private tabService: TabService) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isResetRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path === 'reset';
+    });
+  }
+
+
+  get selectedIndex(): number {
+    return this.tabService.getSelectedIndex();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import { HttpClientModule } from '@angular/common/http';
+import { Login } from './login/login.component';
+import { Register } from './register/register.component';
+import { Reset } from './reset/reset.component';
+import { FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Login,
+    Register,
+    Reset
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule, MatSelectModule, MatFormFieldModule,
+    MatInputModule,MatIconModule,MatButtonModule,MatTabsModule,MatTooltipModule, MatSnackBarModule,MatPasswordStrengthModule,
+    RouterModule.forRoot([
+      { path: 'reset', component: Reset }, 
+      { path: '', redirectTo: '/login', pathMatch: 'full' }, 
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  hide: boolean = true;
+   
+ }
+
