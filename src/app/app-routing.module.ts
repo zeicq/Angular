cc38275a443b245b6908d20541855d31dc9161e8
zeicq <patryk-zielinski@outlook.com>
2023-11-08 20:42:09 +0100

    Add route for reset component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Reset } from './reset/reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reset', component: Reset }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
